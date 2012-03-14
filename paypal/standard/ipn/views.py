7df86d759537907622b948a9022f8a06eb012736
Rick Vause <rvause@gmail.com>
2012-03-14 15:14:59 +0100

    Add csrf_exempt to ipn view

--- paypal/standard/ipn/views.py
+++ paypal/standard/ipn/views.py
@@ -2,17 +2,19 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 from paypal.standard.ipn.forms import PayPalIPNForm
 from paypal.standard.ipn.models import PayPalIPN
 
 
+@csrf_exempt
 @require_POST
 def ipn(request, item_check_callable=None):
     """
     PayPal IPN endpoint (notify_url).
     Used by both PayPal Payments Pro and Payments Standard to confirm transactions.
     http://tinyurl.com/d9vu9d
-    
+
     PayPal IPN Simulator:
     https://developer.paypal.com/cgi-bin/devscr?cmd=_ipn-link-session
     """
@@ -28,7 +30,7 @@ def ipn(request, item_check_callable=None):
         flag = "Invalid form. (%s)" % form.errors
 
     if ipn_obj is None:
-        ipn_obj = PayPalIPN()    
+        ipn_obj = PayPalIPN()
 
     ipn_obj.initialize(request)
 
